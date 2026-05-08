8d3c32f41acb2076773c3e2b6e500b014c1aa0be
System Sourcer <simon@galloway-mielke.de>
2026-05-08 22:50:35 +0200

    test ip qrc

--- moni/main.py
+++ moni/main.py
@@ -20,6 +20,7 @@ def bon(printer, bon): #https://python-escpos.readthedocs.io/en/latest/api/escpo
     printer.cut()
 
 hostname = socket.gethostname()
+print(socket.gethostbyname_ex(hostname))
 ip_addr = socket.gethostbyname_ex(hostname)[2][0]
 qrc = qrcode.QRCode()
 qrc.add_data('http://' + ip_addr + ':8000')
