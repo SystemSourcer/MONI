ddc1d07ae6f2c4096ae93dcd5e389f795d196a3e
System Sourcer <simon@galloway-mielke.de>
2026-05-10 02:48:50 +0200

    fix bug like

--- moni/main.py
+++ moni/main.py
@@ -22,7 +22,7 @@ def print_bon(order_key): #https://python-escpos.readthedocs.io/en/latest/api/es
     keywords = keywords_dict[settings_dict['Bon_Language']]
     printer = Network(settings_dict[f'Printer-{order['category']}'])
     printer.profile.media['width']['pixels'] = 567 # or 384, depending to the printer
-    printer.set(align='center', bold = True, custom_size=True, width=2, height=2)
+    printer.set(align='center', bold = True, custom_size=False, width=2, height=2)
     printer.textln(settings_dict['Event'])
     printer.set(bold=False, width=1, height=1)
     printer.textln(settings_dict['Host'])
@@ -41,7 +41,7 @@ def print_bon(order_key): #https://python-escpos.readthedocs.io/en/latest/api/es
     printer.set(bold=False, width=1, height=1)
     printer.textln("-" * int(settings_dict['Bon_Row_Chars']))
     printer.qr(json.dumps(order, ensure_ascii=False, indent=2), size = 4,  center=True ) # defualt size = 3
-    printer.cut(mode='FULL', feed=False)
+    printer.cut(mode='PART', feed=False)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
